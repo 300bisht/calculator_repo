de03eca7e3d4cee39e7ec88c52147563a8f4b227
sandeep <300bisht@gmail.com>
2025-01-17 13:06:55 +0530

    when the input is a single number

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,8 @@
-# string_calculator.rb
 class Calculator
   def self.add(numbers)
     return 0 if numbers.empty?
+
+    # If there's only one number, return that number itself
+    return numbers.to_i if numbers =~ /^\d+$/
   end
 end
