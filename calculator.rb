a2e2b5e72c391197af00664ce99c115e18976f8f
sandeep <300bisht@gmail.com>
2025-01-17 13:13:30 +0530

    returns the sum of the numbers separated by the multi-character delimiters

--- calculator.rb
+++ calculator.rb
@@ -1,9 +1,11 @@
+# calculator.rb
 class Calculator
   def self.add(numbers)
     return 0 if numbers.empty?
 
-    if numbers.start_with?("//")
-      delimiter, numbers = numbers[2..-1].split("\n", 2)
+    if numbers.start_with?("//[")
+      delimiter = numbers[3..-2]
+      numbers = numbers.split("\n")[1]
       delimiter = Regexp.escape(delimiter)
     else
       delimiter = ","
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,9 +1,9 @@
 require './calculator.rb'
 RSpec.describe Calculator do
   describe '.add' do
-    context 'when a custom delimiter is used' do
-      it 'returns the sum of the numbers separated by the custom delimiter' do
-        expect(Calculator.add("//;\n1;2")).to eq(3)
+    context 'when a multi-character delimiter is used' do
+      it 'returns the sum of the numbers separated by the multi-character delimiter' do
+        expect(Calculator.add("//[***]\n1***2***3")).to eq(6)
       end
     end
   end
